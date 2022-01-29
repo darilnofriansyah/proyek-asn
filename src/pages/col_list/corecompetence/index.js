331136ce915e1fc96cc2a775826d776b71e1e2f4
GDN\m.badruddin <m.badruddin@gdn-commerce.com>
2022-01-29 13:14:04 +0700

    adding description

--- src/pages/col_list/corecompetence/index.js
+++ src/pages/col_list/corecompetence/index.js
@@ -1,9 +1,19 @@
-import { Button, Flex, Heading, Link } from "@chakra-ui/react";
+import {
+  Button,
+  Flex,
+  Heading,
+  Link,
+  List,
+  ListIcon,
+  ListItem,
+  Text,
+} from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import CustomTable from "../../../components/table";
 import Papa from "papaparse";
 import LoadingSpinner from "../../../components/loadingspinner";
 import ColumnFilter from "../../../components/columnfilter";
+import { ArrowRightIcon } from "@chakra-ui/icons";
 
 export default function CoreCompetence() {
   const [data, setData] = useState([]);
@@ -77,7 +87,47 @@ export default function CoreCompetence() {
         <LoadingSpinner />
       ) : (
         <Flex direction="column" pt="4rem" px="4rem">
-          <Heading mb="4rem">Core Competence</Heading>
+          <Heading mb="2rem">Core Competence</Heading>
+          <Text mb="1rem" fontSize="xl">
+            Daftar ini memberikan informasi nama jabatan yang mendukung program
+            prioritas strategis nasional (major project) dan visi presiden. Nama
+            jabatan dibedakan dari jenis jabatannya. Tabel pertama menampilkan
+            nama jabatan dengan jenis JPT, tabel kedua menampilan nama jabatan
+            berdasarkan jenis JF yang sudah ditetapkan dalam Permenpan,
+            sedangkan tabel ketiga merupakan nama jabatan fungsional yang
+            bersifat strategis dan diusulkan menjadi nama jabatan baru untuk
+            memperkuat pelaksanaan program prioritas strategis dan visi
+            presiden.
+          </Text>
+          <Text mb="2rem" fontSize="xl">
+            Dalam Penentuan level kompetensi ditentukan dalam 2 jenis kompetensi
+            yaitu berdasarkan Permenpan no.38 tahun 2017 dan Penyesuaian
+            berdasarkan standar KKNI.
+          </Text>
+          <List mb="4rem" spacing={3}>
+            <ListItem>
+              <ListIcon as={ArrowRightIcon} />
+              Untuk level kompetensi JPT berdasarkan Permenpan no. 38 tahun 2017
+              masuk kedalam level kompetensi 4-5 (JPT Pratama dan Madya)
+              sedangkan level kompetensi SKKNI disetarakan dengan level 8 - 9
+              yaitu lulusan Magister Terapan dan Magister paling rendah setara
+              dengan jenjang 8; lulusan Doktor Terapan dan Doktor setara dengan
+              jenjang 9 dan lulusan pendidikan spesialis setara dengan jenjang 8
+              atau 9.
+            </ListItem>
+            <ListItem>
+              <ListIcon as={ArrowRightIcon} />
+              Untuk level kompetensi JF dan Future jobs berdasarkan permenpan 38
+              tahun 2017 masuk kedalam level kompetensi level 2 - 4 (jenjang
+              Pertama s.d Utama) sedangkan level kompetensi SKKNI disetarakan
+              dengan level 6-9 yaitu lulusan Diploma 4 atau Sarjana Terapan dan
+              Sarjana paling rendah setara dengan jenjang 6; lulusan Magister
+              Terapan dan Magister paling rendah setara dengan jenjang 8;
+              lulusan Doktor Terapan dan Doktor setara dengan jenjang 9; lulusan
+              pendidikan profesi setara dengan jenjang 7 atau 8; lulusan
+              pendidikan spesialis setara dengan jenjang 8 atau 9.
+            </ListItem>
+          </List>
           <CustomTable customColumns={columns} customData={data} />
         </Flex>
       )}
--- src/pages/col_list/jptkritis/index.js
+++ src/pages/col_list/jptkritis/index.js
@@ -1,4 +1,4 @@
-import { Button, Flex, Heading, Link } from "@chakra-ui/react";
+import { Button, Flex, Heading, Link, Text } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import CustomTable from "../../../components/table";
 import Papa from "papaparse";
@@ -6,62 +6,81 @@ import LoadingSpinner from "../../../components/loadingspinner";
 import ColumnFilter from "../../../components/columnfilter";
 
 export default function JPTKritis() {
-    const [data, setData] = useState([]);
-    const [loading, setLoading] = useState(true);
-    useEffect(() => {
-      async function getData() {
-        const response = await fetch("/files/JPT_Kritis.csv");
-        const reader = response.body.getReader();
-        const result = await reader.read(); // raw array
-        const decoder = new TextDecoder("utf-8");
-        const csv = decoder.decode(result.value); // the csv text
-        const results = Papa.parse(csv, { header: true }); // object with { data, errors, meta }
-        const rows = results.data; // array of objects
-        setData(rows);
-        setLoading(false);
-        console.log(rows);
-      }
-      if (loading) {
-        getData();
-      }
-    }, []);
-  
-    const columns = [
-      {
-        Header: "No",
-        accessor: "index",
-        Cell: (props) => {
-          return <div>{props.row.index + 1}</div>;
-        },
-        disableFilters: true
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch("/files/JPT_Kritis.csv");
+      const reader = response.body.getReader();
+      const result = await reader.read(); // raw array
+      const decoder = new TextDecoder("utf-8");
+      const csv = decoder.decode(result.value); // the csv text
+      const results = Papa.parse(csv, { header: true }); // object with { data, errors, meta }
+      const rows = results.data; // array of objects
+      setData(rows);
+      setLoading(false);
+      console.log(rows);
+    }
+    if (loading) {
+      getData();
+    }
+  }, []);
+
+  const columns = [
+    {
+      Header: "No",
+      accessor: "index",
+      Cell: (props) => {
+        return <div>{props.row.index + 1}</div>;
       },
-      {
-        Header: "Nama Jabatan",
-        accessor: "Nama Jabatan",
-        disableFilters: true
-      },
-      {
-        Header: "Instansi Pengampu",
-        accessor: "Instansi Pengampu",
-        Filter: ColumnFilter,
-      },
-      {
-        Header: "Level Pemerintahan",
-        accessor: "Level Pemerintahan",
-        Filter: ColumnFilter,
-      },
-    ];
-  
-    return (
-      <>
-        {loading ? (
-          <LoadingSpinner />
-        ) : (
-          <Flex direction="column" pt="4rem" px="4rem">
-            <Heading mb="4rem">JPT Kritis</Heading>
-            <CustomTable customColumns={columns} customData={data} />
-          </Flex>
-        )}
-      </>
-    );
+      disableFilters: true,
+    },
+    {
+      Header: "Nama Jabatan",
+      accessor: "Nama Jabatan",
+      disableFilters: true,
+    },
+    {
+      Header: "Instansi Pengampu",
+      accessor: "Instansi Pengampu",
+      Filter: ColumnFilter,
+    },
+    {
+      Header: "Level Pemerintahan",
+      accessor: "Level Pemerintahan",
+      Filter: ColumnFilter,
+    },
+  ];
+
+  return (
+    <>
+      {loading ? (
+        <LoadingSpinner />
+      ) : (
+        <Flex direction="column" pt="4rem" px="4rem">
+          <Heading mb="4rem">Daftar Jabatan Kritis JPT</Heading>
+          <Text mb="1rem" fontSize="xl">
+            Data ini merupakan daftar jabatan kritis jenjang JPT yang
+            berkontribusi pada proyek prioritas strategis nasional (Major
+            Project) dan visi Presiden dari akumulasi data pendekatan top down
+            dan bottom up. Data dipisahkan atas jenjang JPT dan dilengkapi
+            dengan instansi pengampu jabatan pimpinan tinggi pada proyek
+            prioritas strategis maupun visi presiden.
+          </Text>
+          <Text mb="4rem" fontSize="xl">
+            Jabatan dengan kategori tertentu artinya diampu oleh
+            kementerian/lembaga khusus, jabatan dengan keterangan instansi pusat
+            menandakan bahwa jabatan tersebut terdapat pada beberapa K/L di
+            Pemerintah Pusat, jabatan dengan keterangan Instansi Daerah
+            menandakan bahwa jabatan tersebut terdapat pada beberapa K/L di
+            Pemerintah Daerah. Sedangkan jabatan dengan keterangan “instansi
+            pusat- instansi daerah” sifatnya terbuka atau kebutuhan akan jabatan
+            tersebut tidak hanya di satu instansi tertentu melainkan ada di
+            Pemerintah Pusat di Daerah.
+          </Text>
+          <CustomTable customColumns={columns} customData={data} />
+        </Flex>
+      )}
+    </>
+  );
 }
